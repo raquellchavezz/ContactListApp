950c2e202ff211d01f1ebcc4d7331dc045eedf03
raquellchavezz <raquel.chavez41313@gmail.com>
2023-03-28 12:27:58 -0700

    updates the student file to reflect contact variable names

--- client/src/components/Student.jsx
+++ client/src/components/Student.jsx
@@ -3,26 +3,26 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import * as ioicons from 'react-icons/io5'
 
-const Student = ({student, toUpdate, toDelete}) => {
+const Contact = ({contact, toUpdate, toDelete}) => {
 
-    const onUpdate = (toUpdateStudent) => {
-        toUpdate(toUpdateStudent)
+    const onUpdate = (toUpdateContact) => {
+        toUpdate(toUpdateContact)
     }
 
-    const onDelete = (toDeleteStudent) => {
-        toDelete(toDeleteStudent)
+    const onDelete = (toDeleteContact) => {
+        toDelete(toDeleteContact)
     }
 
     return (
         <Card>
             <Card.Body>
-            <Card.Title>{student.firstname} {student.lastname}</Card.Title>
-            <Button variant="outline-danger" onClick={()=>{onDelete(student)}} style={{padding: '0.6em', marginRight:'0.9em'}}><ioicons.IoTrash/></Button>
-            <Button variant="outline-info" onClick={()=>{onUpdate(student)}} style={{padding: '0.6em'}}> <ioicons.IoSync/></Button>
+            <Card.Title>{contact.firstname} {contact.lastname}</Card.Title>
+            <Button variant="outline-danger" onClick={()=>{onDelete(contact)}} style={{padding: '0.6em', marginRight:'0.9em'}}><ioicons.IoTrash/></Button>
+            <Button variant="outline-info" onClick={()=>{onUpdate(contact)}} style={{padding: '0.6em'}}> <ioicons.IoSync/></Button>
             </Card.Body>
         </Card>
     )
 
 }
 
-export default Student;
+export default Contact;
